105fcdf75ed01270378113920c385de7ed10d361
Charles Ferguson <gerph@gerph.org>
2025-10-26 15:19:47 +0000

    Copy over the Pyromaniac tests, together with Debugger harness.
    The tests have been copied out of RISC OS Pyromaniac and added to
    the repository. Together with the test harness code we built for
    Debugger, this lets us run the tests on the build service, very
    much like Pyromaniac does.

---
+++ testcode/run-on-build-service.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+##
+# Run on the build service
+#
+
+set -e
+set -o pipefail
+
+quiet=false
+if [[ "$1" == '--quiet' ]] ; then
+    quiet=true
+    shift
+fi
+
+#quiet=false
+
+# We're running only AArch32 at the moment
+arch=aarch32
+
+# How long each job runs for
+timeout=30
+
+# Files we will archive
+files=(SystemVars,ffa
+       SystemVarsDefaults32,ffa
+       bin/var_read,ffc
+       bin/var_set,ffc
+       testdata
+      )
+
+
+# We want to turn all the pyro switches into things that can be run inside the Build service.
+# The switches look like this:
+#   --command "riscos command"
+#   --config pyrosection.option=value
+# We need to turn these into something like:
+#   echo *** Run riscos command
+#   riscos command
+#
+#   echo *** Configure pyrosection.option=value
+#   pyromaniacconfig pyrosection.option=value
+#
+
+lines=()
+
+tail_command=
+nextarg=
+while [[ $# -gt 0 ]] ; do
+    #echo "Checking $1 (nextarg=$nextarg)"
+
+    if [[ "$nextarg" == 'command' ]] ; then
+        if ! $quiet ; then
+            lines+=("echo *** Run $1")
+        fi
+        lines+=("$1")
+        nextarg=
+    elif [[ "$nextarg" == 'config' ]] ; then
+        if ! $quiet ; then
+            lines+=("echo *** Configure $1")
+        fi
+        lines+=("PyromaniacConfig $1")
+        nextarg=
+    elif [[ "$nextarg" == 'set' ]] ; then
+        if ! $quiet ; then
+            lines+=("echo *** Setting $1")
+        fi
+        var=${1%%=*}
+        val=${1#*=}
+        lines+=("Set $var $val")
+        nextarg=
+    else
+        if [[ "$1" == '--command' ]] ; then
+            nextarg='command'
+        elif [[ "$1" == '--config' ]] ; then
+            nextarg='config'
+        elif [[ "$1" == '--set-variable' ]] ; then
+            nextarg='set'
+        elif [[ "${1:0:2}" == '--' ]] ; then
+            echo "Unsupported switch '$1'" >&2
+            exit 1
+        else
+            tail_command="$tail_command $1"
+        fi
+    fi
+    shift
+done
+
+if [[ "$tail_command" != '' ]] ; then
+    lines+=("${tail_command:1:${#tail_command}}")
+fi
+
+if [[ -x './riscos-build-online' ]] ; then
+    build_tool="./riscos-build-online"
+elif type -p riscos-build-online > /dev/null 2>/dev/null ; then
+    build_tool=$(type -p riscos-build-online)
+else
+    echo "The 'riscos-build-online' tool is required to run these tests" >&2
+    exit 1
+fi
+
+    # Header
+cat > .robuild.yaml <<EOM
+%YAML 1.0
+---
+
+# Defines a list of jobs which will be performed.
+# Only 1 job will currently be executed.
+jobs:
+  build:
+    # Env defines system variables which will be used within the environment.
+    # Multiple variables may be assigned.
+    env:
+      "Sys\$Environment": ROBuild
+
+    # Directory to change to before running script
+    dir: "${dir}"
+
+    # Commands which should be executed to perform the build.
+    # The build will terminate if any command returns a non-0 return code or an error.
+    script:
+      - PyromaniacDebug traceblock
+EOM
+
+# Load the module
+line="RMLoad SystemVars"
+if ! $quiet ; then
+    echo "      - echo *** Load module" >> .robuild.yaml
+    echo "      - echo ***   $line" >> .robuild.yaml
+fi
+line="RMLoad SystemVarsDefaults32"
+if ! $quiet ; then
+    echo "      - echo *** Load module" >> .robuild.yaml
+    echo "      - echo ***   $line" >> .robuild.yaml
+fi
+echo "      - $line" >> .robuild.yaml
+
+# Run the necessary command
+for line in "${lines[@]}" ; do
+    echo "      - $line" >> .robuild.yaml
+done
+
+build_args=()
+if $quiet ; then
+    build_args+=(-q)
+fi
+
+# Archive the files we want
+zip -q9r /tmp/testrun.zip "${files[@]}" .robuild.yaml
+
+if [[ -t 0 ]] ; then
+    function tidy_output() {
+        sed -E -e "s/\\r\\r/\\r/g"
+    }
+else
+    function tidy_output() {
+        sed -E -e "s/\\r//g"
+    }
+fi
+
+# And send it off to the build system
+if "$build_tool" "${build_args[@]}" -a off -A "$arch" -t "$timeout" -i /tmp/testrun.zip | tidy_output ; then
+    rc=0
+else
+    rc=$?
+fi
+
+# We don't need the build file any more
+rm .robuild.yaml
+
+exit "$rc"
